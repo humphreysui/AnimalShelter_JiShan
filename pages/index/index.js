61a9662446542d0dae832dba9a74299d305ed146
Humphrey SUI <55611676+humphreysui@users.noreply.github.com>
2022-03-11 22:25:13 +0800

    added more pet info

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,6 +6,15 @@ Page({
   data:{
     list_sm:[
       {
+        picPath: "/images/dogwb2.png",
+        age: 4,
+        gender: "公",
+        weight: 9,
+        cut: true,
+        vax: true,
+        breed: "未知"
+      },
+      {
         picPath: "/images/pet1.png",
         age: 3,
         gender: "公",
@@ -25,10 +34,28 @@ Page({
         
       },
       {
-        picPath: "/images/fish1.jpeg",
-        age: 5,
+        picPath: "/images/dog3.jpeg",
+        age: 2,
+        gender: "母",
+        weight: 10,
+        cut: true,
+        vax: false,
+        breed: "金毛"
+      },
+      {
+        picPath: "/images/dogwb1.png",
+        age: 4,
         gender: "公",
-        weight: 5,
+        weight: 12,
+        cut: true,
+        vax: true,
+        breed: "阿拉斯加"
+      },
+      {
+        picPath: "/images/fish1.jpeg",
+        age: 1,
+        gender: "公",
+        weight: 0.1,
         cut: true,
         vax: true,
         breed: "鱼"
@@ -52,7 +79,25 @@ Page({
         cut: true,
         vax: false,
         breed: "边牧"
-      }
+      },
+      {
+        picPath: "/images/pet4.png",
+        age: 4,
+        gender: "公",
+        weight: 12,
+        cut: true,
+        vax: true,
+        breed: "未知"
+      },
+      {
+        picPath: "/images/pet5.png",
+        age: 4,
+        gender: "公",
+        weight: 2,
+        cut: true,
+        vax: false,
+        breed: "未知"
+      },
     ]
   },
   
